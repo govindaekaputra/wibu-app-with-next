d202ca19f62f2a43edb44f777f2d41478cfe248e
govinda_weirdo <govinda.ekaputra09@gmail.com>
2024-05-29 18:44:19 +0800

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ import Home from "@/app/_components/Home";
 import { getTopAnime } from "@/app/top-anime/_services/top-anime";
 
 export default async function HomePage() {
-  const resultTV = await getTopAnime("tv");
-  const resultMovie = await getTopAnime("movie");
-  const topAnimesTV: Anime[] = resultTV.data;
-  const topAnimesMovie: Anime[] = resultMovie.data;
+  let topAnimesTV: Anime[] = [];
+  let topAnimesMovie: Anime[] = [];
+  try {
+    const resultTV = await getTopAnime("tv");
+    const resultMovie = await getTopAnime("movie");
+    topAnimesTV = resultTV.data;
+    topAnimesMovie = resultMovie.data;
+  } catch (error) {}
   return <Home topAnimesTV={topAnimesTV} topAnimesMovie={topAnimesMovie} />;
 }
